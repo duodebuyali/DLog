28e4de9bf8dc5e1d1253eb0c4a8e9c4e875e9c46
hekang <hekang@codemao.cn>
2020-10-10 17:00:58 +0800

    初步完成查询条件的UI设计

--- logconsole/src/main/java/com/duode/log/logconsole/bean/QueryConfigData.kt
+++ logconsole/src/main/java/com/duode/log/logconsole/bean/QueryConfigData.kt
@@ -8,6 +8,12 @@ import com.duode.log.logconsole.consts.ConsoleConst
  * @author hekang
  * @des 配置console显示的数据以及样式
  * @date 2020/9/16 13:46
+ *
+ * 每个条件，是一个item，每个item配置一些默认选择和自定义选项；
+ * 自定义都是另外弹出一个界面，选择当前输入的数据是前缀或后缀或包含(前后缀都可以)
+ * 每个item的选项，都是当前flag值和全部以及自定义；如果在自定义输入了数据，item的选项会新增这个数据并选中；当关闭预览console之后，清空之前自定义的数据，展示默认选项
+ *
+ * 增加一个排序的选项，按时间倒序或正序；按插入即id倒序或正序
  */
 data class QueryConfigData(
     var queryFlag: Int = ConsoleConst.NONE_MATCH_FLAG,
